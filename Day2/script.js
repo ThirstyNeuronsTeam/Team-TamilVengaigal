55200a075ff4a685d5dd2e62d16b6439fefcab2d
ThirstyNeurons <156573994+ThirstyNeurons@users.noreply.github.com>
2024-05-29 21:59:17 +0530

    Day2 Files

---
+++ Day2/script.js
@@ -0,0 +1,17 @@
+console.log("this is first programming code");
+//pseudocode
+//goal : rendu numbera koduthaa , add panni result vaanganum
+//given : rendu numbers
+//logic : ?
+//output : rendu numberoda kootal
+function sum(number1, number2) {
+  //const let var
+  const result = number1 + number2;
+  return result;
+}
+
+let sumResult = sum(2, 3);
+
+sumResult = 6;
+
+console.log(sumResult);
