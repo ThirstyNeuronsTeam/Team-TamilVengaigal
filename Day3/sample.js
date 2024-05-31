c6b58b874a6eb73245b2154a8441714bfe375d10
ThirstyNeurons <156573994+ThirstyNeurons@users.noreply.github.com>
2024-05-31 22:11:47 +0530

    Day3

---
+++ Day3/sample.js
@@ -0,0 +1,59 @@
+function sum(num1,num2){
+    return num1+num2;
+}
+
+
+//console.time()
+const result = sum(200,36767);
+//console.timeEnd()
+
+//console.log(result)
+
+
+//function
+//const
+//return
+//log
+//console
+//log
+//time
+//timeEnd
+//Function name
+//Variable Name
+//Function Invoketion
+
+
+function doubleTheLetters(name){
+    //count the letters
+    const nameLength = name.length;
+
+    //take the notebook
+    let notebook = "";
+    
+    //loop the letters
+    for(let loopIndex=0;loopIndex<nameLength;loopIndex++){
+        //take letter
+        const letter = name[loopIndex];
+         //write twice in the notebook
+         notebook = notebook + letter + letter
+    }
+
+    return notebook
+        //first loop
+           //take first letter
+           //write twice in the notebook
+        //second iteration
+           //take second letter
+           //write twiic in the notebook next to the previuos letter
+        //Third iteration
+          //take third letter
+          //write twiice in the notebook next to the previuos letter
+       // ... till the last letter
+}
+
+
+const result1 = doubleTheLetters("Ram")
+
+console.log(result1);
+
+//RRaamm
