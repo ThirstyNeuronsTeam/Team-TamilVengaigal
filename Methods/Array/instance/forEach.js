58be1871129c68fadee094f9c0a5974228168458
ThirstyNeurons <156573994+ThirstyNeurons@users.noreply.github.com>
2024-06-14 21:03:33 +0530

    Day8

--- Methods/Array/instance/forEach.js
+++ Methods/Array/instance/forEach.js
@@ -1,31 +1,53 @@
-const { faker ,en_IN , Faker } = require("@faker-js/faker")
+const { faker, en_IN, Faker, de } = require("@faker-js/faker");
 
+//We have constructed factory to generate employee data
 const customFaker = new Faker({ locale: [en_IN] });
 
-const employeeData = Array.from({length:20},()=>{
-    return {
-        id: customFaker.string.uuid(),
-        name : customFaker.person.firstName() + " " + customFaker.person.lastName(),
-        phone : customFaker.phone.number(),
-        email : faker.internet.email(),
-        gender : faker.person.sex()
-    }
-})
-
-function sendMail(emailId,emailContent){
-    console.log("sent mail")
-    console.log(emailId,emailContent)
-    //server logic to send mail
+function generateSingleEmployeeData() {
+  return {
+    id: faker.string.uuid(),
+    name: customFaker.person.firstName() + " " + customFaker.person.lastName(),
+    phone: customFaker.phone.number(),
+    email: faker.internet.email(),
+    gender: faker.person.sex(),
+  };
 }
 
-const emailContent = faker.lorem.paragraph() ;
-console.log(emailContent);
-
-for(let employeeIndex=0;employeeIndex<employeeData.length;employeeIndex++){
-    const currentEmployee = employeeData[employeeIndex]
-    sendMail(currentEmployee.email,faker.lorem.paragraph())
+// Generating Data using the factory
+function generateAllEmployeesData() {
+  return Array.from({ length: 20 }, () => generateSingleEmployeeData());
 }
 
+const employees = generateAllEmployeesData();
+
+//send Mail
+function sendMail(emailId, emailContent) {
+  console.log("sent mail");
+  console.log(emailId, emailContent);
+  //server logic to send mail
+}
+
+//send mail to all employees
+
+function sendMailToAllEmployees(emailContent) {
+  //common email content
+  /* for (
+    let employeeIndex = 0;
+    employeeIndex < employees.length;
+    employeeIndex++
+  ) {
+    const currentEmployee = employees[employeeIndex];
+    //console.log(currentEmployee);
+    sendMail(currentEmployee.email, faker.lorem.paragraph());
+  } */
 
 
+  const sendMailToSelectedEmployee = (singleEmployee) =>
+    sendMail(singleEmployee.email, emailContent);
+  
 
+  employees.forEach(sendMailToSelectedEmployee);
+}
+
+sendMailToAllEmployees("Music event");
+sendMailToAllEmployees("Health Event");
